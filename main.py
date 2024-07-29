b4f4341b8a4dcc11da23e4d0b8559b0bac1e8719
NoobCrewDelux <j0sephw1532@gmail.com>
2024-07-29 23:56:57 +1200

    Create main.py
    Main Menu, game fundamentals

---
+++ main.py
@@ -0,0 +1,105 @@
+import pygame                       # import modules
+from pygame.locals import *
+
+FPS=60                              # game fps
+
+
+FRAMEDIM = 1920,1080                # game constants
+FRAMECENTRE= 960,540
+
+BUTTONSIZE = (200,50)
+
+WHITE = (255,255,255)
+
+running = True
+
+pygame.font.init()
+largeFont = pygame.font.SysFont("consolas",50)
+mediumFont = pygame.font.SysFont("courier-new",25)
+smallFont = pygame.font.SysFont("courier-new",15)
+
+pygame.init()
+clock=pygame.time.Clock()
+
+
+def eventHandler()-> bool:          # Event handler                                              
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                return False
+            
+        if event.type == QUIT:
+            return False
+        
+    return True
+
+
+class Error(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class Label():
+    def __init__(self,pos,text,fontType) -> None:
+        if fontType == "L":
+            self.text = largeFont.render(str(text),True,WHITE)
+
+        elif fontType == "M":
+            self.text = mediumFont.render(str(text),True,WHITE)
+        
+        elif fontType == "S":
+            self.text = smallFont.render(str(text),True,WHITE)
+
+        else:raise Error('Incorrect parameters for "fontType"')
+            
+
+        self.text_rect = self.text.get_rect(center=pos)
+        
+    def blitSelf(self):             # Method to render text onto the screen to reduce cluttering in essential areas
+        screen.blit(self.text,self.text_rect)
+
+
+
+
+
+class Button():
+    def __init__(self,pos,text) -> None:
+        self.surf = pygame.Surface(BUTTONSIZE)
+        self.surf.fill((0,0,0))
+        self.rect = self.surf.get_rect(center=pos)
+        self.text = mediumFont.render(str(text),True,WHITE)
+        self.text_Rect = self.text.get_rect(center=pos)
+
+    def blitSelf(self):
+        screen.blit(self.text,self.text_Rect)
+
+
+
+screen = pygame.display.set_mode(FRAMEDIM)
+
+
+# MENU LABEL ASSIGN
+
+titleLabel = Label((960,240),"Block Dodge","L")
+
+
+# MENU BUTTON ASSIGN
+
+startButtonMain = Button((960,400),"singleplayer")
+
+
+while running:
+
+    running = eventHandler()
+    screen.fill((0,0,0))
+
+    titleLabel.blitSelf()
+
+    startButtonMain.blitSelf()
+
+    screen.blit(startButtonMain.surf,(860,515))
+    
+
+
+
+    pygame.display.flip()
+    clock.tick(FPS)
